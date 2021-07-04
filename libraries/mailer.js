d897ef17c63a4fbe31031d57409d58f0c09488f2
madeyinka007 <mutairadeyinka@yahoo.com>
2021-07-04 22:13:10 +0100

    mail update

--- libraries/mailer.js
+++ libraries/mailer.js
@@ -14,6 +14,7 @@ const MailGun = {
         }
         _client.messages().send(mailOption, (err,response) => {
             if (err){
+                console.log(err)
                 return callback(false)
             }else {
                 return callback(true)
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,7 @@ const { emailTemplate } = require('./../views/templates/contact')
 
 //mailer route
 router.post('/sendmail', async (req, res) => {
-    try {
-        //get form values for mailing...
+ 
         var error = []
         if(!req.body.name)error.push('Provide Name')
         if(!req.body.email)error.push('Provide Email')
@@ -15,22 +14,13 @@ router.post('/sendmail', async (req, res) => {
         if(!req.body.message)error.push('Leave a message')
 
         if (error.length == 0) {
-            //Initiates email sending...
-            const { name, email, subject, message } = req.body
-            await mailer.sendMail({
-                name,
-                email,
-                subject,
-                message: await emailTemplate(name, email, subject, message)
+            mailOption = {name:req.body.name,email:req.body.email,subject:req.body.subject, message:req.body.message}
+            mailer.sendMail(mailOption, (msg) => {
+                if (msg) return res.status(200).json({ type: 'successs'})
+                else return res.status(500).json({ type: 'error'})
             })
-            return res.status(200).json({ type: 'successs'})
-        } else {
-            return res.status(500).json({ type: 'error'})
         }
-    } catch (err) {
-        //console.log(err)
-        return res.status(400).json({ type: err})
-    }
+
 })
 
 module.exports = router
